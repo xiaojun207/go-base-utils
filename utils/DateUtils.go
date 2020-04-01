8a8a8012b6311b69a9f66543af448e8606a7430a
name <email>
2020-04-01 22:43:52 +0800

    add TimeToStr

--- utils/DateUtils.go
+++ utils/DateUtils.go
@@ -16,3 +16,7 @@ func UtcToTime(utcDate string) time.Time {
 	date, _ := time.ParseInLocation("2006-01-02T15:04:05.000Z", utcDate, time.UTC) // "2018-10-21T01:20:18.0Z"
 	return date
 }
+
+func TimeToStr(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
