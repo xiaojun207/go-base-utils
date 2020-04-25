d2bdb625e5591483af8a78a337fa3d63f717276f
name <email>
2020-04-25 23:32:43 +0800

    修改 Float64ToString为默认显示全部小数点

--- utils/stringUtils.go
+++ utils/stringUtils.go
@@ -41,12 +41,11 @@ func ArrayContains(arr []string, s string) bool {
 //float32 转 String工具类，保留6位小数
 func Float32ToString(input_num float32) string {
 	// to convert a float number to a string
-	return strconv.FormatFloat(float64(input_num), 'f', 8, 64)
+	return strconv.FormatFloat(float64(input_num), 'f', -1, 64)
 }
 
 func Float64ToString(input_num float64) string {
-	// to convert a float number to a string
-	return strconv.FormatFloat(input_num, 'f', 8, 64)
+	return Float64ToStr(input_num, -1)
 }
 
 func Float64ToStr(input_num float64, prec int) string {
