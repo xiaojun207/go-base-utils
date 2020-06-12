bed3dd966cc17e9c9c70600695244628fb72071c
name <email>
2020-06-12 15:56:42 +0800

    优化MapToStruct等函数

--- utils/jsonUtils.go
+++ utils/jsonUtils.go
@@ -6,7 +6,17 @@ import (
 	"github.com/goinggo/mapstructure"
 )
 
+// 参数s必须是指针
 func MapToStruct(m interface{}, s interface{}) error {
+	jsonBytes, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(jsonBytes, s)
+	return err
+}
+
+func MapToStruct2(m interface{}, s interface{}) error {
 	err := mapstructure.Decode(m, s)
 	if err != nil {
 		fmt.Println(err)
@@ -15,17 +25,14 @@ func MapToStruct(m interface{}, s interface{}) error {
 	return nil
 }
 
+// 参数m必须是指针
 func StructToMap(s interface{}, m interface{}) error {
 	j, _ := json.Marshal(s)
 	return json.Unmarshal(j, &m)
 }
 
-func MapToJson(instance map[string]interface{}) string {
-	mapInstances := []map[string]interface{}{}
-	mapInstances = append(mapInstances, instance)
-
-	jsonStr, err := json.Marshal(mapInstances)
-
+func MapToJson(instance interface{}) string {
+	jsonStr, err := json.Marshal(instance)
 	if err != nil {
 		fmt.Println("MapToJson err: ", err)
 	}
@@ -48,10 +55,12 @@ func StructToJson(result interface{}) string {
 	return string(jsonBytes)
 }
 
+// 参数result必须是指针
 func JsonToStruct(jsonStr string, result interface{}) {
-	json.Unmarshal([]byte(jsonStr), &result)
+	json.Unmarshal([]byte(jsonStr), result)
 }
 
+// 参数result必须是指针
 func JsonBytesToStruct(jsonByte []byte, result interface{}) {
-	json.Unmarshal(jsonByte, &result)
+	json.Unmarshal(jsonByte, result)
 }
