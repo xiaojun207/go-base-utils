24c8deeecb13ab28c1d49e90031527641452fb57
name <email@example.com>
2021-07-22 11:06:51 +0800

    add Sha256

--- utils/md5.go
+++ utils/md5.go
@@ -2,10 +2,22 @@ package utils
 
 import (
 	"crypto/md5"
-	"fmt"
+	"crypto/sha256"
+	"encoding/hex"
 )
 
+//md5加密
 func Md5(s string) string {
-	data := []byte(s)
-	return fmt.Sprintf("%x", md5.Sum(data))
+	m := md5.New()
+	m.Write([]byte(s))
+	res := hex.EncodeToString(m.Sum(nil))
+	return res
+}
+
+//Sha256加密
+func Sha256(s string) string {
+	m := sha256.New()
+	m.Write([]byte(s))
+	res := hex.EncodeToString(m.Sum(nil))
+	return res
 }
