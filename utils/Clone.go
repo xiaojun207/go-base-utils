4ab8eb6e12058bf5f5448012eca4dabcb7392da8
name <email@example.com>
2022-01-11 08:24:52 +0800

    优化 Clone.go

--- utils/Clone.go
+++ utils/Clone.go
@@ -28,31 +28,37 @@ func Clone(src interface{}) interface{} {
 }
 
 //深度克隆，可以克隆任意数据类型
-func DeepClone(src interface{}) interface{} {
+func DeepClone(src interface{}) (error, interface{}) {
 	typ := reflect.TypeOf(src)
 	if typ.Kind() == reflect.Ptr { //如果是指针类型
-		typ = typ.Elem()                          //获取源实际类型(否则为指针类型)
-		dst := reflect.New(typ).Elem()            //创建对象
-		b, _ := json.Marshal(src)                 //导出json
-		json.Unmarshal(b, dst.Addr().Interface()) //json序列化
-		return dst.Addr().Interface()             //返回指针
+		typ = typ.Elem()               //获取源实际类型(否则为指针类型)
+		dst := reflect.New(typ).Elem() //创建对象
+		b, err := json.Marshal(src)    //导出json
+		if err != nil {
+			return err, nil
+		}
+		err = json.Unmarshal(b, dst.Addr().Interface()) //json序列化
+		return err, dst.Addr().Interface()              //返回指针
 	} else {
-		dst := reflect.New(typ).Elem()            //创建对象
-		b, _ := json.Marshal(src)                 //导出json
-		json.Unmarshal(b, dst.Addr().Interface()) //json序列化
-		return dst.Interface()                    //返回值
+		dst := reflect.New(typ).Elem() //创建对象
+		b, err := json.Marshal(src)    //导出json
+		if err != nil {
+			return err, nil
+		}
+		err = json.Unmarshal(b, dst.Addr().Interface()) //json序列化
+		return err, dst.Interface()                     //返回值
 	}
 }
 
-func NewInterface(typ reflect.Type, data []byte) interface{} {
+func NewInterface(typ reflect.Type, data []byte) (error, interface{}) {
 	if typ.Kind() == reflect.Ptr {
 		typ = typ.Elem()
 		dst := reflect.New(typ).Elem()
-		json.Unmarshal(data, dst.Addr().Interface())
-		return dst.Addr().Interface()
+		err := json.Unmarshal(data, dst.Addr().Interface())
+		return err, dst.Addr().Interface()
 	} else {
 		dst := reflect.New(typ).Elem()
-		json.Unmarshal(data, dst.Addr().Interface())
-		return dst.Interface()
+		err := json.Unmarshal(data, dst.Addr().Interface())
+		return err, dst.Interface()
 	}
 }
